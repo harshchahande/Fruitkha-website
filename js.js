f9b8e48f51c9902d128077d26653f7e5f908f1d6
harshchahande <harshchahande123@gmail.com>
2024-02-16 19:46:04 +0530

    Initial commit

---
+++ js.js
@@ -0,0 +1,21 @@
+var a = document.getElementById("s1-n");
+var b = document.getElementById("s1a");
+var c = document.getElementById("s1a1");
+var d = document.getElementById("s1a2");
+var e = document.getElementById("s1a3");
+var f = document.getElementById("s1a4");
+var g = document.getElementById("s1a5");
+var h = document.getElementById("s1a6");
+var i = document.getElementById("s1a7");
+a.style.backgroundColor = "transparent";
+function fun() {
+  a.style.backgroundColor = "rgb(5,25,34)";
+  b.style.color = "white";
+  c.style.color = "white";
+  d.style.color = "white";
+  e.style.color = "white";
+  f.style.color = "white";
+  g.style.color = "white";
+  h.style.color = "white";
+  i.style.color = "white";
+}
